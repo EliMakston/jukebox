b0934adb80d0bd0048676471350168f25d242f80
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-07-25 11:18:51 -0700

    fixed

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const clientId = '2857db851d23461f82030f8aa666dbdb';
-const clientSecret = '3e282fa25387445da540d3498bf0079d';
 
 const PORT = 8080
 
@@ -17,29 +15,7 @@ class Room {
 }
 
 let queue;
-let playerToken;
 let accessToken;
-
-async function APIController() {
-    async function _getToken() {
-        const result = await fetch('https://accounts.spotify.com/api/token', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-            },
-            body: 'grant_type=client_credentials'
-        });
-
-        const data = await result.json();
-        return data.access_token;
-    }
-
-    const access_token = await _getToken();
-    accessToken = access_token;
-}
-
-//APIController();
 
 app.use(morgan('tiny'));
 
@@ -53,14 +29,14 @@ app.get('/', (req, res) => {
 
 app.post('/queue', async (req, res) => {
     queue = req.body;
-    playerToken = req.query.access;
+    accessToken = req.query.access;
     res.send('Successfully sent queue to server');
 });
 
 app.get('/song', async (req, res) => {
     const searchValue = req.query.search;
     const result = await fetch(`https://api.spotify.com/v1/search?q=${searchValue}&type=track`, {
-        method: "GET", headers: { Authorization: `Bearer ${playerToken}` }
+        method: "GET", headers: { Authorization: `Bearer ${accessToken}` }
     });
     const response = await result.json();
     res.send(response);
@@ -69,7 +45,7 @@ app.get('/song', async (req, res) => {
 app.post('/song', async (req, res) => {
     const trackFromSearch = req.query.songId;
     const result = await fetch(`https://api.spotify.com/v1/me/player/queue?uri=${trackFromSearch}`, {
-        method: "POST", headers: { Authorization: `Bearer ${playerToken}` }
+        method: "POST", headers: { Authorization: `Bearer ${accessToken}` }
     });
     res.send('This worked');
 })
@@ -88,7 +64,7 @@ app.get('/host', (req, res) => {
 
 app.get('/queue', async (req, res) => {
     const result = await fetch("https://api.spotify.com/v1/me/player/queue", {
-        method: "GET", headers: { Authorization: `Bearer ${playerToken}` }
+        method: "GET", headers: { Authorization: `Bearer ${accessToken}` }
     });
     queue = await result.json();
     res.send(queue);
