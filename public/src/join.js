44db9325a157020485cded1ab5d6a86da23d1520
Eli Makston <73192122+EliMakston@users.noreply.github.com>
2023-07-31 21:34:11 -0700

    get playlists

--- public/src/join.js
+++ public/src/join.js
@@ -1,5 +1,5 @@
-const params = window.location.search;
-const urlParams = new URLSearchParams(params);
+const query = window.location.search;
+const urlParams = new URLSearchParams(query);
 const searchForm = document.getElementById('search');
 const searchBar = document.getElementById('song-id');
 
@@ -82,3 +82,92 @@ function populateSearch(songList)  {
 }
 
 populateUI();
+
+const loginButton = document.getElementById('login-button');
+
+const clientId = '2857db851d23461f82030f8aa666dbdb';
+const params = new URLSearchParams(window.location.search);
+const code = params.get("code");
+
+loginButton.addEventListener('click', (e) => {
+    code_check();
+});
+
+if (code) {
+    code_check();
+}
+
+async function code_check() {
+    if (!code) {
+        redirectToAuthCodeFlow(clientId);
+    } else {
+        const accessToken = await getAccessToken(clientId, code);
+        const response = await fetchPlaylists(accessToken);
+        playlists = response;
+        console.log(playlists);
+    }
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:8080/join");
+    params.append("scope", "playlist-read-private");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+async function getAccessToken(clientId, code) {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:8080/join");
+    params.append("code_verifier", verifier);
+
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params
+    });
+
+    const { access_token } = await result.json();
+    return access_token;
+}
+
+async function fetchPlaylists(token) {
+    const result = await fetch("https://api.spotify.com/v1/me/playlists", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+}
